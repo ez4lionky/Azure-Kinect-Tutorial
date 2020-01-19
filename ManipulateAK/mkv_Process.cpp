29b779d13310854ed7aabdc2ec8cff14d9f3c10f
ez4lionky <codex.lxy@gmail.com>
2020-01-19 20:51:17 +0800

    Added several ways to manipulate Azure Kinect and reconstruct.

--- ManipulateAK/mkv_Process.cpp
+++ ManipulateAK/mkv_Process.cpp
@@ -20,143 +20,29 @@ using namespace std;
 #include <k4arecord/playback.h>
 #include <k4abt.h>
 
-using DepthPixel = uint16_t;
-
-struct Pixel
+static cv::Mat color_to_opencv(const k4a::image &im)
 {
-    uint8_t Blue;
-    uint8_t Green;
-    uint8_t Red;
-    uint8_t Alhpa;
-};
-
-using DepthPixelVisualizationFunction = Pixel(const DepthPixel &value, const DepthPixel &min, const DepthPixel &max);
-
-inline std::pair<uint16_t, uint16_t> GetDepthModeRange(const k4a_depth_mode_t depthMode)
-{
-    switch (depthMode)
-    {
-    case K4A_DEPTH_MODE_NFOV_2X2BINNED:
-        return {(uint16_t)500, (uint16_t)5800};
-    case K4A_DEPTH_MODE_NFOV_UNBINNED:
-        return {(uint16_t)500, (uint16_t)4000};
-    case K4A_DEPTH_MODE_WFOV_2X2BINNED:
-        return {(uint16_t)2500, (uint16_t)3000};
-    case K4A_DEPTH_MODE_WFOV_UNBINNED:
-        return {(uint16_t)250, (uint16_t)2500};
-
-    case K4A_DEPTH_MODE_PASSIVE_IR:
-    default:
-        throw logic_error("Invalid depth mode!");
-    }
+    cv::Mat cv_image_with_alpha(im.get_height_pixels(), im.get_width_pixels(), CV_8UC4, (void *)im.get_buffer());
+    cv::Mat cv_image_no_alpha;
+    cv::cvtColor(cv_image_with_alpha, cv_image_no_alpha, cv::COLOR_BGRA2BGR);
+    return cv_image_no_alpha;
 }
 
-inline void ColorConvertHSVtoRGB(float h, float s, float v, float &out_r, float &out_g, float &out_b)
+static cv::Mat depth_to_opencv(const k4a::image &im)
 {
-    if (s == 0.0f)
-    {
-        out_r = out_g = out_b = v;
-        return;
-    }
-
-    h = fmodf(h, 1.0f) / (60.0f / 360.0f);
-    int i = (int)h;
-    float f = h - (float)i;
-    float p = v * (1.0f - s);
-    float q = v * (1.0f - s * f);
-    float t = v * (1.0f - s * (1.0f - f));
-
-    switch (i)
-    {
-    case 0:
-        out_r = v;
-        out_g = t;
-        out_b = p;
-        break;
-    case 1:
-        out_r = q;
-        out_g = v;
-        out_b = p;
-        break;
-    case 2:
-        out_r = p;
-        out_g = v;
-        out_b = t;
-        break;
-    case 3:
-        out_r = p;
-        out_g = q;
-        out_b = v;
-        break;
-    case 4:
-        out_r = t;
-        out_g = p;
-        out_b = v;
-        break;
-    case 5:
-    default:
-        out_r = v;
-        out_g = p;
-        out_b = q;
-        break;
-    }
+    return cv::Mat(im.get_height_pixels(),
+                   im.get_width_pixels(),
+                   CV_16U,
+                   (void *)im.get_buffer(),
+                   static_cast<size_t>(im.get_stride_bytes()));
 }
 
-Pixel ColorizeBlueToRed(const DepthPixel &depthPixel, const DepthPixel &min, const DepthPixel &max)
+static k4a::image create_depth_image_like(const k4a::image &im)
 {
-    constexpr uint8_t PixelMax = std::numeric_limits<uint8_t>::max();
-    Pixel result = {
-        uint8_t(0), uint8_t(0), uint8_t(0), PixelMax};
-    if (depthPixel == 0)
-    {
-        return result;
-    }
-    uint16_t clampedValue = depthPixel;
-    clampedValue = std::min(clampedValue, max);
-    clampedValue = std::max(clampedValue, min);
-
-    float hue = (clampedValue - min) / static_cast<float>(max - min);
-
-    constexpr float range = 2.f / 3.1;
-    hue *= range;
-
-    hue = range - hue;
-
-    float fRed = 0.f;
-    float fGreen = 0.f;
-    float fBlue = 0.f;
-    ColorConvertHSVtoRGB(hue, 1.f, 1.f, fRed, fGreen, fBlue);
-
-    result.Red = static_cast<uint8_t>(fRed * PixelMax);
-    result.Green = static_cast<uint8_t>(fGreen * PixelMax);
-    result.Blue = static_cast<uint8_t>(fBlue * PixelMax);
-
-    return result;
-}
-
-void ColorizeDepthImage(const k4a::image &depthImage,
-                        DepthPixelVisualizationFunction visualizationFn,
-                        std::pair<uint16_t, uint16_t> expectedValueRange,
-                        std::vector<Pixel> *buffer)
-{
-    const k4a_image_format_t imageFormat = depthImage.get_format();
-    if (imageFormat != K4A_IMAGE_FORMAT_DEPTH16 && imageFormat != K4A_IMAGE_FORMAT_IR16)
-    {
-        throw std::logic_error("Attempted to colorize a non-depth image!");
-    }
-    const int width = depthImage.get_width_pixels();
-    const int height = depthImage.get_height_pixels();
-    buffer->resize(static_cast<size_t>(width * height));
-    const uint16_t *depthData = reinterpret_cast<const uint16_t *>(depthImage.get_buffer());
-    for (int h = 0; h < height; ++h)
-    {
-        for (int w = 0; w < width; ++w)
-        {
-            const size_t currentPixel = static_cast<size_t>(h * width + w);
-            (*buffer)[currentPixel] = visualizationFn(depthData[currentPixel],
-                                                      expectedValueRange.first, expectedValueRange.second);
-        }
-    }
+    return k4a::image::create(K4A_IMAGE_FORMAT_DEPTH16,
+                              im.get_width_pixels(),
+                              im.get_height_pixels(),
+                              im.get_width_pixels() * static_cast<int>(sizeof(uint16_t)));
 }
 
 bool check_data_exists(k4a_capture_t capture, const string data_type)
@@ -180,6 +66,7 @@ bool check_data_exists(k4a_capture_t capture, const string data_type)
 
 bool process_mkv_offline(const char *input_path, const char *output_path)
 {
+    uint16_t depth_threshold = 1000;
     k4a_playback_t playback_handle = nullptr;
     k4a_result_t result = k4a_playback_open(input_path, &playback_handle);
     if (result != K4A_RESULT_SUCCEEDED)
@@ -195,12 +82,13 @@ bool process_mkv_offline(const char *input_path, const char *output_path)
         cerr << "Failed to get calibration" << endl;
         return false;
     }
+    k4a::transformation depth_to_color(calibration);
 
     cout << "Reading RGB frame and depth data" << input_path << endl;
 
     int frame_count = 0;
     bool success = true;
-    
+
     while (true)
     {
         k4a_capture_t capture_handle = nullptr;
@@ -208,7 +96,6 @@ bool process_mkv_offline(const char *input_path, const char *output_path)
         k4a_stream_result_t stream_result = k4a_playback_get_next_capture(playback_handle, &capture_handle);
         k4a::image colorImage, depthImage;
         cv::Mat depthFrame, colorFrame;
-        // std::vector<Pixel> depthTextureBuffer;
 
         if (stream_result == K4A_STREAM_RESULT_EOF)
         {
@@ -222,22 +109,24 @@ bool process_mkv_offline(const char *input_path, const char *output_path)
             {
                 colorImage_handle = k4a_capture_get_color_image(capture_handle);
                 colorImage = k4a::image(colorImage_handle);
-                // depthImage_handle = k4a_capture_get_depth_image(capture_handle);
-                // depthImage = k4a::image(depthImage_handle);
+                depthImage_handle = k4a_capture_get_depth_image(capture_handle);
+                depthImage = k4a::image(depthImage_handle);
 
-                // ColorizeDepthImage(depthImage,
-                //                    ColorizeBlueToRed,
-                //                    GetDepthModeRange(K4A_DEPTH_MODE_NFOV_UNBINNED),
-                //                    &depthTextureBuffer);
+                k4a::image depth_in_color = create_depth_image_like(colorImage);
+                // Create a blank image size like colorImage, which used as depth_in_color image
+                depth_to_color.depth_image_to_color_camera(depthImage, &depth_in_color);
+                // Transforms the depth map into the geometry of the color camera.
+                // depth_in_color is the transformed_depth_image
 
-                uint8_t *colorTextureBuffer = colorImage.get_buffer();
+                cv::Mat depthFrame = depth_to_opencv(depth_in_color);
+                cv::Mat colorFrame = color_to_opencv(colorImage);
 
-                // cv::Mat depthFrame = cv::Mat(depthImage.get_height_pixels(), depthImage.get_width_pixels(), CV_8UC4, &depthTextureBuffer[0]);
-                cv::Mat colorFrame = cv::Mat(colorImage.get_height_pixels(), colorImage.get_width_pixels(), CV_8UC4, colorTextureBuffer);
-
-                // imshow("Depth image of frame: " + to_string(frame_count), depthFrame);
+                cv::Mat within_threshold_range = (depthFrame != 0) &
+                                             (depthFrame < depth_threshold);
+                colorFrame.copyTo(colorFrame, within_threshold_range);
+                imshow("Depth image of frame: " + to_string(frame_count), depthFrame);
                 imshow("Color image of frame: " + to_string(frame_count), colorFrame);
-                
+
                 cvWaitKey(0);
                 k4a_capture_release(capture_handle);
             }
@@ -251,23 +140,6 @@ bool process_mkv_offline(const char *input_path, const char *output_path)
         frame_count++;
     }
 };
-
-static cv::Mat color_to_opencv(const k4a::image &im)
-{
-    cv::Mat cv_image_with_alpha(im.get_height_pixels(), im.get_width_pixels(), CV_8UC4, (void *)im.get_buffer());
-    cv::Mat cv_image_no_alpha;
-    cv::cvtColor(cv_image_with_alpha, cv_image_no_alpha, cv::COLOR_BGRA2BGR);
-    return cv_image_no_alpha;
-}
-
-static cv::Mat depth_to_opencv(const k4a::image &im)
-{
-    return cv::Mat(im.get_height_pixels(),
-                   im.get_width_pixels(),
-                   CV_16U,
-                   (void *)im.get_buffer(),
-                   static_cast<size_t>(im.get_stride_bytes()));
-}
 
 int main(int argc, char **argv)
 {
--- intro-AK-SDK.md
+++ intro-AK-SDK.md
@@ -2,8 +2,22 @@
 
 #### The pipeline of Azure Kinect and related code
 Pipeline of rgb-d data collection: 
-Power on = startDevice() -> 
-Capture RGB-D data = k4a_capture_t capture_handle (A capture represents a set of images that were captured by a device at approximately the same time. A capture may have a color, IR, and depth image, [link](https://microsoft.github.io/Azure-Kinect-Sensor-SDK/release/1.3.x/structk4a__capture__t.html#details)) ->
-After 
 
+1. refer to [blog](https://cloud.tencent.com/developer/article/1535784), collect data in real-time.
+Power on = k4a::device::open() -> dev.start_cameras()
+Capture RGB-D data = k4a_capture_t capture_handle (A capture represents a set of images that were captured by a device at approximately the same time. A capture may have a color, IR, and depth image, [link](https://microsoft.github.io/Azure-Kinect-Sensor-SDK/release/1.3.x/structk4a__capture__t.html#details)) ->
+Write color image and depth image = capture.get_color_image() / capture.get_depth_image() -> cv::imwrite() ->
+Power off = dev.close()
+(The code is in file ManipulateAK.cpp, but it is not complete)
+2. refer to [official example](https://github.com/microsoft/Azure-Kinect-Samples/tree/master/body-tracking-samples/offline_processor), using playback() function to read mkv file.
+(The code is in file mkv_Process.cpp, but the result is strange)
+
+#### The easier way
+
+1. Recording rgbd data by using Azure Kinect SDK -> .mkv file
+
+2. Using [API](http://www.open3d.org/docs/latest/tutorial/Basic/azure_kinect.html?highlight=azure%20kinect) (azure_kinect_mkv_reader.py) of python package Open3D to process mkv file.
+
+3. Using reconstruction [module](http://www.open3d.org/docs/latest/tutorial/ReconstructionSystem/capture_your_own_dataset.html#) in Open3D.
+For reconstruction, [tsdf-fusion](https://github.com/andyzeng/tsdf-fusion-python) also can be used, but need to have related RT. The module in Open3D is recommended.
 
